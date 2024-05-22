38e9303c1d2341076835f601cd0d66593e013f9a
Mohan Zhang <Mohan-Zhang-u@users.noreply.github.com>
2024-05-22 16:04:34 -0400

    update

---
+++ download_models.py
@@ -0,0 +1,6 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
